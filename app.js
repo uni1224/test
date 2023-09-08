24fce2b2452159c7145915727c8e3140dc53344a
uni124 <32253217+uni1224@users.noreply.github.com>
2023-09-08 18:10:20 +0900

    commit2

--- app.js
+++ app.js
@@ -3,4 +3,36 @@ const ansewrs = [
     'スーパーファミコン',
     'プレイステーション2',
     'ニンテンドースイッチ',
-    'ニンテンドーDS'];
+    'ニンテンドーDS'
+];
+const correct = 'ニンテンドーDS';
+const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
+//クイズの問題文、選択肢
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent =question;
+    let buttonIndex = 0;
+    while(buttonIndex < buttonLength) {
+      $button[buttonIndex].textContent =ansewrs[buttonIndex];
+      buttonIndex++;
+}
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+    if (correct === e.target.textContent) {
+        window.alert('正解！');
+    } else {
+        window.alert('不正解！');
+    }
+};
+let hanlerIndex= 0;
+while(hanlerIndex < buttonLength) {
+    $button[hanlerIndex].addEventListener('click',(e) => {
+        clickHandler(e);
+});
+hanlerIndex++;
+}
+
+
+//ボタンをクリックしたら正誤判定
