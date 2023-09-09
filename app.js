12fb9a65d86ffc4e9d74f07f8fddc98c5a54423e
uni1224 <saekahane99@gmail.com>
2023-09-09 11:24:55 +0900

    クイズゲーム完成

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
-const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
-const ansewrs = [
-    'スーパーファミコン',
-    'プレイステーション2',
-    'ニンテンドースイッチ',
-    'ニンテンドーDS'
+const quiz = [
+    {
+        question: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
+        ansewrs: [
+            'スーパーファミコン',
+            'プレイステーション2',
+            'ニンテンドースイッチ',
+            'ニンテンドーDS'
+        ],
+        correct: 'ニンテンドーDS'
+    },{
+        question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+        ansewrs: [
+            'MOTHER2',
+            'スーパーマリオブラザーズ3',
+            'スーパードンキーコング',
+            '星のカービィー'
+        ],
+        correct: 'MOTHER2'
+    },{
+        question: 'ファイナルファンタジーIVの主人公の名前は？',
+        ansewrs: [
+            'フリオニール',
+            'クラウド',
+            'ティーダ',
+            'セシル'
+        ],
+        correct: 'セシル'
+    }
 ];
-const correct = 'ニンテンドーDS';
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 //クイズの問題文、選択肢
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent =question;
+    document.getElementById('js-question').textContent =quiz[quizIndex].question;
     let buttonIndex = 0;
     while(buttonIndex < buttonLength) {
-      $button[buttonIndex].textContent =ansewrs[buttonIndex];
+      $button[buttonIndex].textContent =quiz[quizIndex].ansewrs[buttonIndex];
       buttonIndex++;
 }
 }
 setupQuiz();
 
 const clickHandler = (e) => {
-    if (correct === e.target.textContent) {
+    if (quiz[quizIndex].correct === e.target.textContent) {
         window.alert('正解！');
+        score++;
     } else {
         window.alert('不正解！');
+    }
+    quizIndex++;
+
+    if(quizIndex < quizLength){
+        //問題数がまだあればコチラを実行
+        setupQuiz();
+    } else {
+        //問題数がもうなければコチラを実行
+        window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です!');
     }
 };
 let hanlerIndex= 0;
